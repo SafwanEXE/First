1ec7da89af1428b735350d2b1af1a8bbd247e194
hayasamaana <haya.sammaneh@gmail.com>
2017-10-28 20:11:08 +0300

    Update MainApp.java

--- src/main/java/converter/MainApp.java
+++ src/main/java/converter/MainApp.java
@@ -1,7 +1,6 @@
 package main.java.converter;
 
 public class MainApp {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(new PigLatin().convertWord("donia"));
