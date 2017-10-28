421d4ed36706007b3f49373ce2efe920f9be24e0
hayasamaana <haya.sammaneh@gmail.com>
2017-10-28 14:19:09 +0300

    initial

---
+++ src/main/java/converter/PigLatin.java
@@ -0,0 +1,19 @@
+package main.java.converter;
+
+public class PigLatin {
+	
+	public String convertWord(String word){
+		word=word.toLowerCase();
+		String _newWord=null;
+		char initial=word.charAt(0);
+		
+		if(initial =='a'||initial =='u'||initial =='e'||initial=='o'||initial=='i') {
+			_newWord=word.concat("way");
+		}
+		else {
+		_newWord=word.substring(1)+word.charAt(0)+"ay";
+		}
+		return _newWord;
+	}
+
+}
---
+++ src/main/test/ConvertTest.java
@@ -0,0 +1,85 @@
+package main.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import main.java.converter.PigLatin;
+@RunWith(Parameterized.class)
+public class ConvertTest {
+	
+	private static PigLatin pig;
+	private String expected,input;
+	/**
+	 * 
+	 * @param in
+	 * @param exp
+	 * Or we can replace constructor with
+	 * //@Parameter // first data value (0) is default 
+	 * public  (NOT private)  String input;
+	 * @Parameter(1)
+	 * public (NOT private) String expected;
+	 * **/
+	
+	public ConvertTest(String in,String exp) {
+		// TODO Auto-generated constructor stub
+		input=in;
+		expected=exp;
+	}
+	
+	
+  
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		pig=new PigLatin();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		/**
+		 * Prepare for Garbage Collector
+		 */
+		pig=null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("I am in a Test method");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("I finished a Test method");
+	}
+
+	@Parameters
+	public static Collection<Object []> inputData(){
+		// order is important  input expected and should match constructor
+		Object [][] data= {{ "donia", "oniaday" }, { "sami", "amisay" }, { "som", "omsay" }};
+		List<Object[]> listOfArrays=Arrays.asList(data);
+		return listOfArrays;
+		
+	}
+	@Test
+	public void conventionalTest() {
+		assertEquals("oniaday", pig.convertWord("donia"));
+	}
+	@Test
+	public void parametrizedTest() {
+		assertEquals(expected,pig.convertWord(input));
+		
+	}
+	
+
+}
